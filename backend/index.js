eae7da71b4892ecc40e4f9432b8b75f1aa5bda9a
satish723 <kandregulasatish2006@gmail.com>
2026-01-27 20:45:02 +0530

    after frontend layout

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 import express from "express";
 import mongoose from 'mongoose';
-import {PORT,mongoDbUrl} from "./config.js"
+import {PORT} from "./config.js"
 import route from './routes/bookroute.js'
+import cors from 'cors';
+import dotenv from 'dotenv';    
+dotenv.config();
 const app=express();
 app.get('/',(req,res)=>
-{
-    res.status(200).send("route page");
-})
-app.use(express.json());
-app.use('/book',route)
+    {
+        res.status(200).send("route page");
+    })
+    app.use(express.json());
+    app.use(cors())
+    app.use('/book',route)
 async function main(){
     try{
-        await mongoose.connect(mongoDbUrl);
+        await mongoose.connect(process.env.mongoDbUrl);
         console.log("success in connection")
         app.listen(PORT,()=>
         {
--- backend/routes/bookroute.js
+++ backend/routes/bookroute.js
@@ -23,11 +23,63 @@ route.post('/',async (req,res)=>
         res.status(500).send({message:err.message});
     }
 })
+route.put('/:id',async (req,res)=>
+{
+    try{
+        if(!req.body)
+        {
+            return res.status(400).send({
+            message:"unsuccesfully created"
+        });
+        }
+        const id=req.params.id;
+        const f=await book.findById(id);
+        if(f){
+        await book.findByIdAndUpdate(id,req.body);
+        res.status(201).send({
+            message:"succesfully created"
+        })
+    }
+    else{
+        res.status(404).send("id is not found")
+    }
+    }
+    catch(err)
+    {
+        res.status(500).send({message:err.message});
+    }
+})
 route.get('/',async (req,res)=>
 {
 try{
     const result=await book.find({});
+    res.status(200).send({length:result.length , data:result})
+}catch(err)
+{
+    res.status(500).send({message:err.message});
+}
+})
+route.get('/:id',async (req,res)=>
+{
+try{
+    let id=req.params.id;
+    const result=await book.findById(id);
     res.status(200).send(result)
+}catch(err)
+{
+    res.status(500).send({message:err.message});
+}
+}) 
+route.delete('/:id',async (req,res)=>
+{
+try{
+    const id=req.params.id;
+    const deletebook = await book.findByIdAndDelete(id);
+    if(!deletebook)
+    {
+        res.status(404).send({message:"not found"});
+    }
+    res.status(200).send({"message":"succesfully deleted"});
 }catch(err)
 {
     res.status(500).send({message:err.message});
